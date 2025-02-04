d41fccd33998c7862b6fabd4429492eb15c3a341
020226 <mmin08@naver.com>
2025-02-04 17:40:48 +0900

    MysqlUtil 도입하여 소스코드 다이어트

--- src/main/java/com/ex/jdbc/board/Article.java
+++ src/main/java/com/ex/jdbc/board/Article.java
@@ -1,6 +1,7 @@
 package com.ex.jdbc.board;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 public class Article {
   int id;
@@ -14,11 +15,12 @@ public class Article {
     this.subject = subject;
     this.content = content;
   }
-  public Article(int id, LocalDateTime regDate, LocalDateTime updateDate, String subject, String content) {    this.id = id;
-    this.regDate = regDate;
-    this.updateDate = updateDate;
-    this.subject = subject;
-    this.content = content;
+  public Article(Map<String, Object> articleMap) {
+    this.id = (int) articleMap.get("id");
+    this.regDate = (LocalDateTime) articleMap.get("regDate");
+    this.updateDate = (LocalDateTime) articleMap.get("updateDate");
+    this.subject = (String) articleMap.get("subject");
+    this.content = (String) articleMap.get("content");
   }
   @Override
   public String toString() {
